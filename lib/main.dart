37dfd9329a1e9f8c0dd570fb6c07061a7e6097bc
Binod Bhandari <binodcoder@gmail.com>
2025-11-14 16:37:47 +0000

    some task to do

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  // TODO: change color of text
+  // TODO: change text
+  // TODO: understand [Container] widget and make use of most of it
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Center(
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: Container(color: Colors.amber, child: Text("Archana Sharma")),
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Container(color: Colors.amber, child: Text("Archana Sharma")),
+          ),
         ),
       ),
     );
