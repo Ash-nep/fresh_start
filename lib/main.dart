5e6a449f806ec08b212c77f930d2261d4d0a7c6d
Ash-nep <asharma.it.nepal@gmail.com>
2025-11-20 07:31:10 +0545

    Properties of constraints

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,21 @@ class MyApp extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.all(8),
             child: Container(
-              width: 300.0,
-              height: 100.0,
+              //width: 300.0,
+              //height: 100.0,
               padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.all(10.0),
+              constraints: BoxConstraints.tightForFinite(
+                width: 300.0,
+                height: 100.0,
+              ),
+              transform: Matrix4.rotationZ(0.05),
+              final Decoration? foregroundDecoration;
               decoration: BoxDecoration(
                 //color: Colors.blueGrey,
                 border: Border.all(color: Colors.green),
               ),
+
               child: Center(
                 child: Text("Stranger Things", textAlign: TextAlign.center),
               ),
